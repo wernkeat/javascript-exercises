c51fffbd569ee02ae8c45403d1a5a1df7c878703
wernkeat <173651601+wernkeat@users.noreply.github.com>
2025-01-03 17:55:21 +1100

    complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.length > 0 ? arr.reduce((a, b) => a * b, 1) : 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num > 1) {
+    for (let i = num - 1; i > 1; i--) {
+      num *= i;
+    }
+
+    return num;
+  }
+
+  return 1;
 };
 
 // Do not edit below this line
