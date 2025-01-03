83e757e84ab894bdfdda44045ff185fe51c48b26
wernkeat <173651601+wernkeat@users.noreply.github.com>
2025-01-03 19:04:24 +1100

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
 
+    return death - birth;
+}
+
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, current) => {
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+
+        return oldestAge > currentAge ? oldest : current;
+    });
 };
 
 // Do not edit below this line
