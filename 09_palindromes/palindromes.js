6b09e8874f6bed7f310ae11ee03409a682e5e247
wernkeat <173651601+wernkeat@users.noreply.github.com>
2025-01-03 18:14:26 +1100

    complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const string = str.toLowerCase().split("").filter(letter => alphanumerical.includes(letter)).join('');
+    const reversed = string.split("").reverse().join("");
+
+    console.log(string);
+    console.log(reversed);
+
+    return string === reversed;
 };
 
 // Do not edit below this line
