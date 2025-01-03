907f0dfafb28208915089ddde60cdeda794d4d70
wernkeat <173651601+wernkeat@users.noreply.github.com>
2025-01-03 18:29:38 +1100

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const n = parseInt(num);
+    if (n < 0) {
+        return "OOPS";
+    } 
+    else if (n === 0) {
+        return 0;
+    }
 
+    let prev2 = 0;
+    let prev = 1;
+
+    for (let i = 2; i <= n; i++) {
+        let current = prev + prev2;
+        prev2 = prev;
+        prev = current; 
+    }
+
+    return prev;
 };
 
 // Do not edit below this line
